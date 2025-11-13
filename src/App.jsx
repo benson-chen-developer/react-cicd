93aebdb87afee6814272a0e70f3e8bc0519ca1a8
benson-chen-developer <bensonchen456@gmail.com>
2025-11-13 00:28:24 -0500

    deploy to docker

--- src/App.jsx
+++ src/App.jsx
@@ -16,20 +16,14 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Whats good benson!</h1>
       <h1 data-testid="title">React Test Demo</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           increment
         </button>
         <p data-testid="count">count is {count}</p>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
